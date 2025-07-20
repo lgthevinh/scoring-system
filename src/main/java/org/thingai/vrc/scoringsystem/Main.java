1edf193060c186a06ba3dd3a0ef0855189cf3d1a
lgthvinh <everwellmax@gmail.com>
2025-07-20 17:18:59 +0700

    update main: - init app path and files

---
+++ src/main/java/org/thingai/vrc/scoringsystem/Main.java
@@ -0,0 +1,33 @@
+package org.thingai.vrc.scoringsystem;
+
+import org.thingai.vrc.scoringsystem.database.Database;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) {
+        String home = System.getProperty("user.home");
+        Path configDir = Paths.get(home, ".thingai", "scoringsystem");
+        Path databaseFile = configDir.resolve("database.db");
+
+        // Ensure the configuration directory exists
+        try {
+            if (Files.notExists(configDir)) {
+                Files.createDirectories(configDir);
+            }
+
+            if (Files.notExists(databaseFile)) {
+                Files.createFile(databaseFile);
+            }
+
+
+        } catch (Exception e) {
+            System.err.println("Could not insert configuration directory: " + e.getMessage());
+            return;
+        }
+
+        Database.DB_URL = "jdbc:sqlite:" + databaseFile.toAbsolutePath();
+    }
+}
