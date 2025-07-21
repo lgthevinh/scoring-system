ae9a677b155603db4f8bb170fb1120efede05c77
lgthvinh <everwellmax@gmail.com>
2025-07-21 11:28:47 +0700

    test

--- src/test/java/TestMatchGenerator.java
+++ src/test/java/TestMatchGenerator.java
@@ -1,13 +1,28 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.thingai.vrc.scoringsystem.annotations.DaoField;
 import org.thingai.vrc.scoringsystem.database.Database;
+import org.thingai.vrc.scoringsystem.model.score.Score;
 import org.thingai.vrc.scoringsystem.model.score.ScoreFactory;
 import org.thingai.vrc.scoringsystem.service.MatchGenerator;
-import org.thingai.vrc.scoringsystem.types.MatchType;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TestMatchGenerator {
 
     private static final MatchGenerator matchGenerator = new MatchGenerator();
+
+    private Field[] getAllFields(Class<?> clazz) {
+        List<Field> fields = new ArrayList<>();
+        while (clazz != null) {
+            fields.addAll(Arrays.asList(clazz.getDeclaredFields()));
+            clazz = clazz.getSuperclass();
+        }
+        return fields.toArray(new Field[0]);
+    }
 
     @BeforeAll
     public static void setup() {
@@ -23,6 +38,23 @@ public class TestMatchGenerator {
         // Assuming a MatchGenerator class exists with a method to generate matches
 
         matchGenerator.generateMatches();
+    }
+
+    @Test
+    public void test() {
+        Score score = ScoreFactory.createScore();
+
+        Field[] fields = getAllFields(score.getClass());
+
+        System.out.println("Fields in " + score.getClass().getSimpleName() + ":");
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(DaoField.class)) {
+                DaoField daoField = field.getAnnotation(DaoField.class);
+                System.out.println("Field: " + field.getName() + ", DaoField name: " + daoField.name());
+            } else {
+                System.out.println("Field: " + field.getName() + " (no DaoField annotation)");
+            }
+        }
     }
 
 }
