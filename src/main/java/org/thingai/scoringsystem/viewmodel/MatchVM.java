8cb513759c5fa4523889ee0475d16fe13cb6c515
lgthvinh <everwellmax@gmail.com>
2025-08-10 22:28:30 +0700

    implement match view model

--- src/main/java/org/thingai/scoringsystem/viewmodel/MatchVM.java
+++ src/main/java/org/thingai/scoringsystem/viewmodel/MatchVM.java
@@ -1,5 +1,7 @@
 package org.thingai.scoringsystem.viewmodel;
 
+import org.thingai.base.dao.Dao;
+import org.thingai.base.dao.DaoFactory;
 import org.thingai.scoringsystem.entity.match.Match;
 import org.thingai.scoringsystem.entity.match.MatchAlliance;
 import org.thingai.scoringsystem.entity.score.Score;
@@ -12,4 +14,51 @@ public class MatchVM {
 
     private Score redScore;
     private Score blueScore;
+
+    public static class MatchVMBuilder {
+        private final MatchVM matchVM;
+        private String matchId;
+        private boolean withTeamInfos = false;
+        private boolean withScores = false;
+
+        public MatchVMBuilder() {
+            matchVM = new MatchVM();
+        }
+
+        public MatchVMBuilder withMatch(String id) {
+            this.matchId = id;
+            return this;
+        }
+
+        public MatchVMBuilder withTeamInfos() {
+            this.withTeamInfos = true;
+            return this;
+        }
+
+        public MatchVMBuilder withScores() {
+            this.withScores = true;
+            return this;
+        }
+
+        public MatchVM build() {
+            Dao<Match, String> matchDao = DaoFactory.getDao();
+
+            matchVM.match = matchDao.read(matchId);
+            if (matchVM.match == null) {
+                throw new RuntimeException("Match not found with ID: " + matchId);
+            }
+
+            if (withTeamInfos) {
+                Dao<MatchAlliance, String> allianceDao = DaoFactory.getDao();
+                matchVM.redAlliance = allianceDao
+                        .query("SELECT * FROM match_alliance WHERE match_id = '" + matchId + "' AND color = 0")
+                        .get(0);
+                matchVM.blueAlliance = allianceDao
+                        .query("SELECT * FROM match_alliance WHERE match_id = '" + matchId + "' AND color = 1")
+                        .get(0);
+            }
+
+            return matchVM;
+        }
+    }
 }
