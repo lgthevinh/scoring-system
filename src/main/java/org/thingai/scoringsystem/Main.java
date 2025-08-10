fbc7064786625bbd413be9adbac35458afdc9dc5
lgthvinh <everwellmax@gmail.com>
2025-08-10 22:28:59 +0700

    refactor with application

--- src/main/java/org/thingai/scoringsystem/Main.java
+++ src/main/java/org/thingai/scoringsystem/Main.java
@@ -1,30 +1,21 @@
 package org.thingai.scoringsystem;
 
+import org.thingai.base.dao.Dao;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
+    private static ScoringApplication scoringApplication = new ScoringApplication();
+
     public static void main(String[] args) {
-        String home = System.getProperty("user.home");
-        Path configDir = Paths.get(home, ".thingai", "scoringsystem");
-        Path databaseFile = configDir.resolve("database.db");
+        scoringApplication.name = "Scoring System";
+        scoringApplication.appDirName = "scoring_system";
+        scoringApplication.version = "1.0.0";
+        scoringApplication.daoType = Dao.SQLITE; // Default DAO type, can be overridden
 
-        // Ensure the configuration directory exists
-        try {
-            if (Files.notExists(configDir)) {
-                Files.createDirectories(configDir);
-            }
-
-            if (Files.notExists(databaseFile)) {
-                Files.createFile(databaseFile);
-            }
-
-
-        } catch (Exception e) {
-            System.err.println("Could not insert configuration directory: " + e.getMessage());
-            return;
-        }
-
+        scoringApplication.init();
+        scoringApplication.start();
     }
 }
---
+++ src/main/java/org/thingai/scoringsystem/ScoringApplication.java
@@ -0,0 +1,17 @@
+package org.thingai.scoringsystem;
+
+import org.thingai.base.Application;
+
+public class ScoringApplication extends Application {
+
+    @Override
+    public void initImpl() {
+        System.out.println("Initializing Scoring Application...");
+    }
+
+    @Override
+    public void startImpl() {
+
+    }
+
+}
