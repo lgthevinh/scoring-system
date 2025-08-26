e79f162604d293bd9f29d540d106bbb7b7ca8934
lgthvinh <everwellmax@gmail.com>
2025-08-26 23:49:09 +0700

    implement new service

--- src/main/java/org/thingai/scoringsystem/Main.java
+++ src/main/java/org/thingai/scoringsystem/Main.java
@@ -2,37 +2,29 @@ package org.thingai.scoringsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.thingai.base.Application;
+import org.thingai.base.Service;
 import org.thingai.base.dao.Dao;
 import org.thingai.scoringsystem.entity.AuthData;
 import org.thingai.scoringsystem.entity.DbMap;
 import org.thingai.scoringsystem.entity.match.Match;
 import org.thingai.scoringsystem.entity.match.MatchAlliance;
-import org.thingai.scoringsystem.entity.score.ScoreSeasonDemo;
 import org.thingai.scoringsystem.entity.team.Team;
 
 @SpringBootApplication
 public class Main {
-    private static final Application scoringApplication = new Application();
 
     public static void main(String[] args) {
-        Application.name = "Scoring System";
-        Application.appDirName = "scoring_system";
-        Application.version = "1.0.0";
-        Application.daoType = Dao.SQLITE; // Default DAO type, can be overridden
+        ScoringService.name = "Scoring System";
+        ScoringService.appDirName = "scoring_system";
+        ScoringService.version = "1.0.0";
+        ScoringService.daoType = Dao.SQLITE; // Default DAO type, can be overridden
 
-        scoringApplication.init();
-        scoringApplication.start();
+        ScoringService scoringService = new ScoringService();
 
-        scoringApplication.facDao(new Class[] {
-                Match.class,
-                MatchAlliance.class,
-//                ScoreSeasonDemo.class,
-                Team.class,
-                AuthData.class,
-                DbMap.class
-        });
+        scoringService.init();
 
+        scoringService.run();
         SpringApplication.run(Main.class, args);
     }
+
 }
