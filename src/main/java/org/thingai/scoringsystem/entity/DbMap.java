62387e26786f7b976eb54bd8d62caa55f67518cf
lgthvinh <everwellmax@gmail.com>
2025-08-26 16:58:56 +0700

    add getter and setter for dbmap

--- src/main/java/org/thingai/scoringsystem/entity/DbMap.java
+++ src/main/java/org/thingai/scoringsystem/entity/DbMap.java
@@ -10,4 +10,20 @@ public class DbMap {
 
     @DaoColumn(nullable = false)
     private String value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
