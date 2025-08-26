4414a14dac6a596d7e1facc6267b6c0e71fb780c
lgthvinh <everwellmax@gmail.com>
2025-08-27 02:11:05 +0700

    add dependences

--- applicationbase/build.gradle.kts
+++ applicationbase/build.gradle.kts
@@ -13,6 +13,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
+    implementation("org.xerial:sqlite-jdbc:3.43.2.0")
+    implementation("org.slf4j:slf4j-api:2.0.9") // Logging interface of jdbc
+
     implementation("com.fasterxml.jackson.core:jackson-core:2.17.2")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.17.2")
