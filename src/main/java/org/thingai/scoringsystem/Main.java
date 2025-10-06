9fc2d99c43b61cf97f08f04ee7fcda250f5bd85a
lgthvinh <everwellmax@gmail.com>
2025-10-07 02:39:29 +0700

    remove unused Dao import and update VCS mappings for applicationbase

--- src/main/java/org/thingai/scoringsystem/Main.java
+++ src/main/java/org/thingai/scoringsystem/Main.java
@@ -2,7 +2,6 @@ package org.thingai.scoringsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.thingai.base.dao.Dao;
 
 @SpringBootApplication
 public class Main {
