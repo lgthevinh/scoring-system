ccea4eb55df7013a1c120892f5bab94eef40e4f4
lgthvinh <everwellmax@gmail.com>
2025-10-22 07:48:50 +0700

    feat: add new error codes for system failures and custom errors

--- src/main/java/org/thingai/app/scoringservice/define/ErrorCode.java
+++ src/main/java/org/thingai/app/scoringservice/define/ErrorCode.java
@@ -1,9 +1,15 @@
 package org.thingai.app.scoringservice.define;
 
 public class ErrorCode {
+    // DAO ERROR CODE
     public static final int CREATE_FAILED = 101;
     public static final int UPDATE_FAILED = 102;
     public static final int DELETE_FAILED = 103;
     public static final int RETRIEVE_FAILED = 104;
     public static final int NOT_FOUND = 105;
+
+    // SYSTEM CODE
+    public static final int FAILED_TO_START = 111;
+
+    public static final int CUSTOM_ERR = 255;
 }
