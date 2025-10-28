824b7a156520d5ec5cf45eb9c58d653b6abe8777
lgthvinh <everwellmax@gmail.com>
2025-10-28 15:12:44 +0700

    feat: add CreateAccount component with form, validation, and unit tests

---
+++ webui/src/app/features/event-dashboard/create-account/create-account.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import {AccountRoleType} from "../../../core/define/AccounRoleType";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {AuthService} from '../../../core/services/auth.service';
+
+@Component({
+  selector: 'app-create-account',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './create-account.html',
+  styleUrl: './create-account.css'
+})
+export class CreateAccount {
+  protected readonly AccountRoleType = AccountRoleType;
+  protected passwordVisible: boolean = false;
+
+  createAccountForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {
+    this.createAccountForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      reEnterPassword: ['', Validators.required],
+      role: [0, Validators.required]
+    });
+  }
+
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  onSubmit() {
+    const formValues = this.createAccountForm.value;
+    if (this.validatePasswords(formValues.password, formValues.reEnterPassword)) {
+      // Proceed with account creation logic
+      console.log('Account creation data:', formValues);
+      let credentials = {
+        username: formValues.username,
+        password: formValues.password,
+        role: formValues.role
+      }
+      this.authService.createAccount(credentials).subscribe({
+        next() {
+          console.log('Account created successfully.');
+        },
+        error(err) {
+          console.error('Error creating account:', err);
+        },
+        complete() {
+          console.log('Account creation request completed.');
+        }
+      })
+    } else {
+      // Handle password mismatch
+      console.error('Passwords do not match.');
+    }
+  }
+
+  validatePasswords(password: string, confirmPassword: string): boolean {
+    return password === confirmPassword;
+  }
+
+}
